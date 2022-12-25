0d69b04b6ef740ceb116c332a3d124eacd620d14
Shawal Mbalire <mbalireshawal@gmail.com>
2022-12-25 16:39:53 +0300

    added testimonials

--- public/static/javascript/index.js
+++ public/static/javascript/index.js
@@ -9,13 +9,45 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*================================= MIXITUP FILTER PORTFOLIO ==========================================*/
-
-
+let mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 /* ======================================Link active work ============================================== */
+const linkWork = document.querySelectorAll('.work__item')
 
+function activeWork() {
+    if(linkWork) {
+        linkWork.forEach(l => l.classList.remove('active-work'))
+        this.classList.add('active-work')
+    }
+}
+linkWork.forEach(l => l.addEventListener('click', activeWork))
 
 /*============================================ SWIPER TESTIMONIAL ======================================*/
-
+var swiperTestimonial = new Swiper(".testimonial__container", {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 24,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 48,
+        },
+    }
+});
 
 /*======================================= SCROLL SECTIONS ACTIVE LINK ===================================*/
 
