61aeecc9daa6506cc5d9b274e08fad9b58b73140
Shawal Mbalire <mbalireshawal@gmail.com>
2023-01-17 20:28:00 +0300

    near finish

--- public/static/javascript/index.js
+++ public/static/javascript/index.js
@@ -1,3 +1,4 @@
+import 'boxicons'
 /* ========================================= CHANGE BACKGROUND HEADER =============================== */
 function scrollHeader() {
     const header = document.getElementById('header')
@@ -6,7 +7,26 @@ function scrollHeader() {
 }
 window.addEventListener('scroll', scrollHeader)
 /*======================================= SERVICES MODAL ==============================================*/
+const modalViews = document.querySelectorAll('.services__modal'),
+      modalBtns  = document.querySelectorAll('.services__button'),
+      modalClose = document.querySelectorAll('.services__modal-close')
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add('active-modal')
+}
 
+modalBtns.forEach((mb,i)=>{
+    mb.addEventListener('click',()=>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc)=>{
+    mc.addEventListener('click',()=>{
+        modalViews.forEach((mv)=>{
+            mv.classList.remove('active-modal')
+        })
+    })
+})
 
 /*================================= MIXITUP FILTER PORTFOLIO ==========================================*/
 let mixerPortfolio = mixitup('.work__container', {
@@ -50,9 +70,64 @@ var swiperTestimonial = new Swiper(".testimonial__container", {
 });
 
 /*======================================= SCROLL SECTIONS ACTIVE LINK ===================================*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*========================================= LIGHT DARK THEME =============================================*/
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'bx-sun'
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the light-theme class
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx bx-moon' : 'bx bx-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+  themeButton.classList[selectedIcon === 'bx bx-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the light / icon theme
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*======================================= SCROLL REVEAL ANIMATION =========================================*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true,
+})
+sr.reveal(`.home__data`)
+sr.reveal(`.home__handle`, {delay:700})
+sr.reveal(`.home__handle, .home__scroll`, {delay:900,origin:'bottom'})
