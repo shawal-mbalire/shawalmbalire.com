0017a492e6ac0716e96ca753f5e9b366189cd2a5
shawal-mbalire <mbalireshawal@gmail.com>
2025-01-15 13:30:46 +0300

    added color pallete

--- portfolio/src/app/work-entry/work-entry.component.ts
+++ portfolio/src/app/work-entry/work-entry.component.ts
@@ -7,6 +7,19 @@ import { WorkEntry } from "../workEntry";
   templateUrl: './work-entry.component.html',
   styleUrl: './work-entry.component.css'
 })
+//const dialog = document.querySelector("dialog");
+//const showButton = document.querySelector("dialog + button");
+//const closeButton = document.querySelector("dialog button");
+//
+//// "Show the dialog" button opens the dialog modally
+//showButton.addEventListener("click", () => {
+//  dialog.showModal();
+//});
+//
+//// "Close" button closes the dialog
+//closeButton.addEventListener("click", () => {
+//  dialog.close();
+//});
 export class WorkEntryComponent {
   @Input() workEntry!:WorkEntry;
 }
