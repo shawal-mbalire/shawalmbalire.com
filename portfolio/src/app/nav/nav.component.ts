6cb6fd32459e7cf6ebe3429014ef30024870a35d
shawal-mbalire <mbalireshawal@gmail.com>
2025-01-20 00:44:30 +0300

    fixed horizontal scroll on smaller phones and links

--- portfolio/src/app/nav/nav.component.ts
+++ portfolio/src/app/nav/nav.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+// import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   imports: [
-    RouterLink,
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
